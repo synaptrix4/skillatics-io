14317a36056fb8ab97c3f262053ac87c9f80421d
shreyas250704 <paranjapeshreyas77@gmail.com>
2026-03-03 16:41:37 +0530

    Added gitignore

--- backend/check_db.py
+++
@@ -1,8 +0,0 @@
-import os
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://127.0.0.1:27017/')
-db = client.skillatics
-
-for q in db.questions.find({'type': 'coding'}):
-    print(f"Title: {q.get('text')} | Function: {q.get('function_name')} | Format: {q.get('input_format')}")
